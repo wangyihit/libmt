0344667275baf4cc5a2816f8f4b665e86f498a92
Yi Wang <wangyihit@hotmail.com>
2019-03-29 14:38:39 +0800

    update proxy checker

--- util/nettools/proxy_checker/proxy_checker.go
+++ util/nettools/proxy_checker/proxy_checker.go
@@ -1,70 +1,69 @@
-package nettools
+package proxy_checker
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"net/url"
-	"errors"
 	"time"
 )
 
 const (
-	HostCheckProxy = 1
+	HostCheckProxy      = 1
 	HostCheckHttpServer = 1 << 1
 
 	HostCheckStatusSuccess = 0
-	HostCheckStatusFailed = 1
+	HostCheckStatusFailed  = 1
 
-	HostHasProxy = 1
+	HostHasProxy      = 1
 	HostHasHttpServer = 2
 
-	ProxyTypeHttp = 1
-	ProxyTypeHttps = 2
+	ProxyTypeHttp   = 1
+	ProxyTypeHttps  = 2
 	proxyTypeSocks5 = 3
 )
-type HostChecker struct {
 
+type HostChecker struct {
 }
+
+var DefaultCheckURL = "https://www.baidu.com"
+var DefaultProxyIP = "109.161.48.228"
+var DefaultProxyPort = 53281
 
 func NewHostChechker() *HostChecker {
 	c := &HostChecker{}
 	return c
 }
 
-
 type HostCheckResult struct {
-	Status      int
-	ProxyStatus int
-	ProxyPort   int
-	ServerStatus      int
-	Message     string
+	Status       int
+	ProxyStatus  int
+	ProxyPort    int
+	ProxyHost    string
+	ServerStatus int
+	Message      string
+	ReqDelay     int64
 }
 
-func NewHostCheckResult()* HostCheckResult {
-	h = & HostCheckResult{
-		Status:      0,
-		ProxyStatus: -1,
-		ServerStatus: -1,
-		ProxyPort:   0,
-		Message:     "",
-
+func NewHostCheckResult() *HostCheckResult {
+	h := &HostCheckResult{
+		Status:       0,
+		ProxyStatus:  0,
+		ServerStatus: 0,
+		ProxyPort:    0,
+		ProxyHost:    "",
+		Message:      "",
+		ReqDelay:     0,
 	}
+	return h
 }
-var DefaultCheckURL = "http://120.92.79.94/api/feed/"
-var proxyIP = "109.161.48.228"
-var proxyPort = 53281
 
-
-func (c *HostChecker) CheckIP(host string, port int){
-
-}
-func (c *HostChecker) BuildProxyUri(host string, port int, uriType int) (error, string){
+func (c *HostChecker) BuildProxyUri(host string, port int, uriType int) (error, string) {
 	var uri string
 	switch uriType {
 	case ProxyTypeHttp:
 		uri = fmt.Sprintf("http://%s:%d", host, port)
-		return nil ,uri
+		return nil, uri
 	case proxyTypeSocks5:
 		uri = fmt.Sprintf("http://%s:%d", host, port)
 		return nil, uri
@@ -73,16 +72,19 @@ func (c *HostChecker) BuildProxyUri(host string, port int, uriType int) (error, 
 	}
 
 }
-func (c *HostChecker)CheckProxy(proxyHost string, proxyPort int, proxyType int, checkURL string, timeout int) *HostCheckResult {
+
+func (c *HostChecker) CheckProxy(proxyHost string, proxyPort int, proxyType int, checkURL string, timeout int) *HostCheckResult {
 	hostCheckResult := NewHostCheckResult()
 	err, proxyUri := c.BuildProxyUri(proxyHost, proxyPort, proxyType)
+	hostCheckResult.ProxyHost = proxyHost
+	hostCheckResult.ProxyPort = proxyPort
 	if err != nil {
 		hostCheckResult.Status = -1
 		return hostCheckResult
 	}
 	proxyURL, err := url.Parse(proxyUri)
 	if err != nil {
-		hostCheckResult.Status = -1
+		hostCheckResult.Status = -2
 		hostCheckResult.Message = fmt.Sprintf("msg=param_error, host=%s, port=%d", proxyHost, proxyPort)
 		return hostCheckResult
 	}
@@ -92,19 +94,27 @@ func (c *HostChecker)CheckProxy(proxyHost string, proxyPort int, proxyType int, 
 	//adding the Transport object to the http Client
 	client := &http.Client{
 		Transport: transport,
-		Timeout: time.Duration(timeout) * time.Second,
+		Timeout:   time.Duration(timeout) * time.Second,
 	}
 	//generating the HTTP GET request
 	request, err := http.NewRequest("GET", checkURL, nil)
 	if err != nil {
-
+		hostCheckResult.Status = -3
+		hostCheckResult.Message = fmt.Sprintf("msg=build_requset_failed, host=%s, port=%d", proxyHost, proxyPort)
+		return hostCheckResult
 	}
 
 	//calling the URL
-	response, err := client.Do(request)
+	start := time.Now().UnixNano() / 1000000
+	_, err = client.Do(request)
+	hostCheckResult.Status = 0
 	if err != nil {
-		log.Println(err)
+		hostCheckResult.Message = fmt.Sprintf("msg=check_proxy_connect_failed, host=%s, port=%d", proxyHost, proxyPort)
+		return hostCheckResult
 	}
-	fmt.Println(response.Status)
+	end := time.Now().UnixNano() / 1000000
+	hostCheckResult.ReqDelay = end - start
+	hostCheckResult.ProxyStatus = 1
+
 	return hostCheckResult
 }
