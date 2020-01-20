93d5e6cd9fd2cdc59736f1b1eee925a35bee6c6f
wangyi <wangyihit@hotmail.com>
2020-01-20 23:25:48 +0800

    update redis options, add db number and url

--- util/options/redis_options/redis.go
+++ util/options/redis_options/redis.go
@@ -1,26 +1,41 @@
 package redis_options
+
 import (
 	"flag"
+	"fmt"
 )
+
 type Options struct {
-	Host string
-	Port int64
+	Host     string
+	Port     int64
 	Password string
+	Url      string
+	DB       int
 }
 
-func NewOptions(host string, port int64, password string, regArgs bool) *Options {
-	options := &Options{
-		Host:host,
-		Port:port,
-		Password:password,
+func toUrl(o *Options) string {
+	// redis://[password@]host[port][/db_num]
+	password := o.Password
+	if password != "" {
+		password = password + "@"
 	}
+	return fmt.Sprintf("redis://$s%s:%d/0", password, o.Host, o.Port)
+}
+
+func NewOptions(host string, port int64, password string, db int, regArgs bool) *Options {
+	options := &Options{
+		Host:     host,
+		Port:     port,
+		Password: password,
+		DB:       db,
+	}
+	options.Url = toUrl(options)
 	if regArgs == false {
 		return options
 	}
 	flag.StringVar(&options.Host, "redis_host", host, "redis host ip")
 	flag.Int64Var(&options.Port, "redis_port", port, "redis port")
 	flag.StringVar(&options.Password, "redis_Password", password, "redis Password")
+	flag.IntVar(&options.DB, "redis_db", db, "redis db number, 0-15")
 	return options
 }
-
-
